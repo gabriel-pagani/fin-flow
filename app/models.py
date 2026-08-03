f8d9e95eefd617e641e58ac4681c446a9a312f7f
Gabriel Pagani <gabrielpaganidesouza@gmail.com>
2026-08-02 22:42:36 -0300

    Add models for Account, Type, Method, Category, and Transaction

--- app/models.py
+++ app/models.py
@@ -21,3 +21,55 @@ class Group(BaseGroup):
         verbose_name = BaseGroup._meta.verbose_name
         verbose_name_plural = BaseGroup._meta.verbose_name_plural
         app_label = 'app'
+
+
+class Account(models.Model):
+    description = models.CharField(max_length=100, unique=True, verbose_name='Conta')
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        ordering = ['description']
+        verbose_name = 'Conta'
+        verbose_name_plural = 'Contas'
+
+
+class Type(models.Model):
+    description = models.CharField(max_length=100, unique=True, verbose_name='Tipo')
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        ordering = ['description']
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+
+
+class Method(models.Model):
+    description = models.CharField(max_length=100, unique=True, verbose_name='Método')
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        ordering = ['description']
+        verbose_name = 'Método'
+        verbose_name_plural = 'Métodos'
+
+
+class Category(models.Model):
+    description = models.CharField(max_length=100, unique=True, verbose_name='Categoria')
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        ordering = ['description']
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+
+class Transaction(models.Model):
+    ...
