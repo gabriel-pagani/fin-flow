9bbae30c84482da00baeea57a0a3bfbeeb43f66a
Gabriel Pagani <gabrielpaganidesouza@gmail.com>
2026-08-07 20:41:45 -0300

    Add custom Argon2 password hasher and update settings; include necessary dependencies

--- project/settings.py
+++ project/settings.py
@@ -68,6 +68,10 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = [
+    "project.hashers.CustomArgon2PasswordHasher",
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
